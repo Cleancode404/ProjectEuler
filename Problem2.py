ce4bd7d4458641a103afb60a63a519be8f5dbffb
Heuristic <codemonkey2468@hotmail.com>
2021-02-14 13:05:26 -0500

    finish problem 2

--- Problem2.py
+++ Problem2.py
@@ -9,4 +9,25 @@
 #
 
 
+#create fibonacci sequence function with recurssion but it is too slow
 
+# def fibonacci_seq(n):
+#     if n == 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return fibonacci_seq(n-2) + fibonacci_seq(n-1)
+
+# print(fibonacci_seq(6))
+a, b, c = 1, 1, 0
+sum = 0
+while c < 4000000:
+    c = a + b
+    if c %2 == 0:
+        sum = sum + c
+
+    a =b
+    b = c 
+
+print(sum)
