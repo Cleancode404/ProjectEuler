e7b5fbc9eb3e36a408bf18d489b35c5773e97cca
Heuristic <codemonkey2468@hotmail.com>
2021-02-15 09:24:10 -0500

    Finish Problem 4: Largest Palindrome Product

--- Problem4.py
+++ Problem4.py
@@ -4,3 +4,25 @@
 #  is 9009 = 91 × 99.
 
 #Find the largest palindrome made from the product of two 3-digit numbers.
+
+#check if an integer is palindrome 
+def isPalindrome(n):
+    s = str(n)
+    reverseString = ""
+
+    for i in range(len(s) - 1, -1, -1):
+        reverseString += s[i]
+    return reverseString == s
+
+#find out the product of two 3-digit numbers
+def product():
+    palindrome = -1
+    for i in range(999, 99, -1):
+        for j in range(i, 99, -1):
+
+            #check if current palindrome is greater than previous one
+            if isPalindrome(i*j) and (i*j) > palindrome:
+                palinedrome = i*j
+    return palinedrome
+
+print(product())
