c79557176720e2a520bbf7b8fa50a3cc699f5c91
Heuristic <codemonkey2468@hotmail.com>
2021-02-15 09:15:58 -0500

    finish problem 7: 10001st Prime

--- Problem7.py
+++ Problem7.py
@@ -3,3 +3,24 @@
 # we can see that the 6th prime is 13.
 # What is the 10 001st prime number?
 #
+
+#check if a number is prime
+def isPrime(n):
+    if n < 2:
+        return "Not a prime"
+    for i in range(2, int(n**.5) + 1 ):
+        if n % i == 0:
+            return False
+    return True
+
+#find out the nth prime
+def nthPrime(n):
+    term = 0
+    prime = 1
+    while term < n:
+        prime = prime + 1
+        if isPrime(prime):
+            term = term + 1
+    return prime
+
+print(nthPrime(10001))
