c2fcfcc3c7193cacde97621b48e7e5c732d46141
Heuristic <codemonkey2468@hotmail.com>
2021-02-16 13:15:12 -0500

    finish problem 16

--- Problem16.py
+++ Problem16.py
@@ -3,5 +3,20 @@
 #2**15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 # What is the sum of the digits of the number 2**1000?#
 
-print(2**1000)
+import time
+#find out how long it will take to compute
+start = time.time()
 
+def sum_digit(n):
+    sum = 0
+    while n >= 1:
+        n, reminder = n//10, n%10
+        sum = sum + reminder
+    return sum
+
+print(sum_digit(2**1000))
+
+
+end  = time.time()
+
+print (end - start)
