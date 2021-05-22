c9c1200ce4f1e6e68b2c7d307d63db4a113f0ffd
Heuristic <codemonkey2468@hotmail.com>
2021-05-22 13:19:31 -0400

    finish problem 19

--- Problem19.py
+++ Problem19.py
@@ -17,3 +17,15 @@ How many Sundays fell on the first of the month during the twentieth century
 (1 Jan 1901 to 31 Dec 2000)?
 """
 
+if __name__ == '__main__':
+    count = 0
+    for i in range(1901, 2001):
+        for j in range(1, 13):
+            if j <= 2:
+                j += 12
+                i -= 1
+            w = i // 100 // 4 - 2 * i // 100 + i % 100 + i % 100 // 4 + 13 * (j + 1) // 5
+            w = (w % 7 + 7) % 7
+            if w == 0:
+                count += 1
+print(count)
