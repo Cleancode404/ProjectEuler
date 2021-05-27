fa9120f800113dbdacd8acc23178f3a9dcc36927
Heuristic <codemonkey2468@hotmail.com>
2021-05-27 17:57:54 -0400

    finish problem 91

--- Problem91.py
+++ Problem91.py
@@ -1,3 +1,21 @@
 """
 Right triangles with integer coordinates
 """
+
+import time
+
+
+if __name__ == '__main__':
+    start = time.clock()
+    N = 50
+    answer = N * N
+    for x1 in range(N + 1):
+        for y1 in range(N + 1):
+            for x2 in range(N + 1):
+                for y2 in range(N + 1):
+                    if x1 == 0 and y1 == 0 or x2 == 0 and y2 == 0:
+                        continue
+                    if x1 ** 2 + y1 ** 2 - x1 * x2 - y1 * y2 == 0 and (x1 != x2 or y1 != y2):
+                        answer += 1
+    print(answer)
+    print('Runtime is', time.clock() - start)
