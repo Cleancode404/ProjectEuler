b0fe30eec0176b82a85c1a71e0f2d8deb50f1b27
Heuristic <codemonkey2468@hotmail.com>
2021-06-04 15:38:08 -0400

    finish problem 38

--- Problem38.py
+++ Problem38.py
@@ -18,3 +18,17 @@ that can be formed as the concatenated product of
 an integer with (1,2, ... , n) where n > 1?
 
 """""
+
+if __name__ == '__main__':
+    res = 0
+    for i in range(10000):
+        s = str(i)
+        k = 2
+        while len(s) < 9:
+            s += str(i * k)
+            k += 1
+        if sorted(s) == ['1', '2', '3', '4', '5', '6', '7', '8', '9'] and res < int(s):
+            res = int(s)
+            print('i = ', i, '( 1, ..., ', k - 1, ')', 'multiples = int(s)')
+    print(res)
+
