f6a53ce8ae492705716fec837caad0cc78846f92
Heuristic <codemonkey2468@hotmail.com>
2021-06-05 12:14:24 -0400

    finish problem 50

--- Problem50.py
+++ Problem50.py
@@ -17,3 +17,28 @@ Which prime, below one-million, can be written as the
 """""
 
 
+if __name__ == '__main__':
+    N = 1000000
+    prime = [True] * (N)
+    prime[0] = prime[1] = False
+    for i in range(2, int(N ** 0.5) + 1):
+        for j in range(i * i, N, i):
+            prime[j] = False
+
+    pp = []
+    for i in range(N):
+        if prime[i]:
+            pp.append(i)
+
+    s = 0
+    cnt = 0
+    for i in range(len(pp)):
+        sum_of_prime = 0
+        for j in range(i, len(pp)):
+            if sum_of_prime + pp[j] >= N:
+                break
+            sum_of_prime += pp[j]
+            if prime[sum_of_prime] and cnt < j - i + 1:
+                s = sum_of_prime
+                cnt = j - i + 1
+    print(s)
