8faddbf5ffdffe9c2cace2d12d0cbb84cdd66879
Heuristic <codemonkey2468@hotmail.com>
2021-06-08 15:56:11 -0400

    finish problem 76

--- Problem76.py
+++ Problem76.py
@@ -2,3 +2,11 @@
 counting summations
 """
 
+if __name__ == '__main__':
+    N = 100
+    ways = [0] * (N + 1)
+    ways[0] = 1
+    for i in range(1, N):
+        for j in range(i, N + 1):
+            ways[j] += ways[j - i]
+    print(ways[N])
