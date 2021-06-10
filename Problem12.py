334668c1cd85e181b36d38005072d37048a03d08
Heuristic <codemonkey2468@hotmail.com>
2021-06-10 12:01:22 -0400

    finish problem 12

--- Problem12.py
+++ Problem12.py
@@ -6,3 +6,29 @@
 # What is the value of the first triangle number to have
 #  over five hundred divisors?#
 
+import time 
+
+def number_of_divisors(n):
+    divisors = 1
+    count = 0
+    while n % 2 == 0:
+        count += 1
+        n //= 2
+    divisors *= (count + 1)
+    p = 3
+    while n != 1:
+        count = 0
+        while n % p == 0:
+            count += 1
+            n //= p
+        divisors *= (count + 1)
+        p += 2
+    return divisors
+
+
+if __name__ == '__main__':
+    start = time.process_time()
+    i = 1
+    while number_of_divisors(i * (i + 1) // 2) <= 500:
+        i += 1
+    print(i * (i + 1) // 2,'\n','Runtime is', time.process_time() - start)
