56f5dba1d4a57f67cbd17e80a8b2f35f8e1fd6ea
Heuristic <codemonkey2468@hotmail.com>
2021-06-15 14:08:40 -0400

    finish problem 115

--- Problem115.py
+++ Problem115.py
@@ -2,3 +2,21 @@
 Counting block combinations II
 
 """
+
+def f(m, n):
+    ways = [0] * (n + 1)
+    for i in range(m):
+        ways[i] = 1
+        
+    for i in range(m, n + 1):
+        ways[i] += ways[i - 1] + 1
+        for j in range(m, i):
+            ways[i] += ways[i - j -1]
+    return ways[n]
+
+
+if __name__ == "__main__":
+    i = 50
+    while f(50, i) <= 1000000:
+        i += 1
+    print(i)
