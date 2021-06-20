109f90bb9a20f892921387108d51858b10a0af39
Heuristic <codemonkey2468@hotmail.com>
2021-06-20 11:30:29 -0400

    finish problem 57

--- Problem57.py
+++ Problem57.py
@@ -1,1 +1,8 @@
 
+if __name__ == 'main':
+    numerator, denominator, count, K = 1, 1, 0, 1000
+    for i in range(K):
+        numberator, denominator = numerator + 2 * denominator, numerator + denominator
+        if len(str(numerator)) > len(str(denominator)):
+            count += 1
+    print(count)
