d93244e5275769f36e9170cc143f6a01c71a5ac4
Heuristic <codemonkey2468@hotmail.com>
2021-09-29 15:29:57 -0400

    finish problem 119

--- Problem119.py
+++ Problem119.py
@@ -2,3 +2,18 @@
 Digit power sum
 
 """
+import time
+
+def digit_sum(n):
+    return sum(map(int, str(n)))
+
+if __name__ == '__main__':
+    start = time.process_time()
+    array = []
+    for base in range(2, 100):
+        for e in range(2, 30):
+            p = base ** e
+            if digit_sum(p) == base:
+                array.append(p)
+    print(sorted(array)[29])
+    print("Runtime is", time.process_time() - start, "seconds")
