d203db7f80542473189f361521e241594062feec
Heuristic <codemonkey2468@hotmail.com>
2021-09-29 15:25:26 -0400

    finish problem 125

--- Problem125.py
+++ Problem125.py
@@ -3,3 +3,21 @@ Palindromic sums
 
 """
 
+import time
+
+if __name__ == '__main__':
+    start = time.process_time()
+    N = 100000000
+    square = [x ** 2 for x in range(1, int(N ** 0.5) + 1)]
+    result = 0
+    palindromic = []
+    for i in range(len(square)):
+        for j in range(i + 2, len(square)):
+            s = sum(square[i:j])
+            if s >= N:
+                break
+            if str(s) == str(s)[::-1] and s not in palindromic:
+                palindromic.append(s)
+                result += s
+    print(result)
+    print("Runtime is ", time.process_time() - start, "seconds")
