5715501152e62c930bb595b3171a102cf30abd44
Heuristic <codemonkey2468@hotmail.com>
2021-09-29 15:26:55 -0400

    Correct problem 123. Done

--- Problem123.py
+++ Problem123.py
@@ -5,20 +5,16 @@ Prime square remainders
 
 import time
 
+
+
+import time
+from pe069 import prime_sieve
+
 if __name__ == '__main__':
     start = time.process_time()
-    N = 100000000
-    square = [x ** 2 for x in range(1, int(N ** 0.5) + 1)]
-    result = 0
-    palindromic = []
-    for i in range(len(square)):
-        for j in range(i + 2, len(square)):
-            s = sum(square[i:j])
-            if s >= N:
-                break
-            if str(s) == str(s)[::-1] and s not in palindromic:
-                palindromic.append(s)
-                result += s
-    print(result)
-    print("Runtime is ", time.process_time() - start, "seconds")
-
+    prime = prime_sieve(300000)
+    for i in range(0, len(prime), 2):
+        if 2 * (i + 1) * prime[i] % prime[i] ** 2 > 10000000000:
+            print(i + 1)
+            break
+    print('Runtime is', time.process_time() - start)
