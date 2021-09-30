e03e8fd1fcbe130a96e335488b1c6ccbf51dfcee
Heuristic <codemonkey2468@hotmail.com>
2021-09-30 16:34:28 -0400

    finish problem 120

--- Problem120.py
+++ Problem120.py
@@ -2,3 +2,11 @@
 Square remainders
 
 """
+
+if __name__ == '__main__':
+    result = 0
+    for i in range(3, 1001):
+        result += i * i - i
+        if i % 2 == 0:
+            result -= i
+    print(result)
