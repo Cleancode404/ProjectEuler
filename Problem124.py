759d659a83edf4f40585f784f08a5c57b24f66d8
Heuristic <codemonkey2468@hotmail.com>
2021-11-03 14:56:45 -0400

    finish problem 124

--- Problem124.py
+++ Problem124.py
@@ -2,3 +2,19 @@
 Ordered radicals
 
 """
+
+def compute(x):
+    limit = 100000
+
+    rads = [0] + [1]* limit
+    for i in range(2, len(rads)):
+        if rads[i] == 1:
+            for j in range(i, len(rads), i):
+                rads[j] *= i
+
+    data = sorted((rads, i) for (i, rad) in enumerate(rads))
+    return str(data[1000][1])
+
+
+if __name__ =="__main__":
+    print(compute(10000))
