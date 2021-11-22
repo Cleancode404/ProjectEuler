df129cee56a14b410f4ed8be3443c0c97ea8b7d3
Heuristic <codemonkey2468@hotmail.com>
2021-11-22 12:55:45 -0500

    finish problem 174

--- Problem174.py
+++ Problem174.py
@@ -2,3 +2,39 @@
 Counting the number of "hollow" square laminae that can form 
 one, two, three, ... distinct arrangements
 """
+
+num = 0
+limit = 10000000
+max_tiles = 1000000
+
+
+count = {}
+
+max_n = 15
+
+
+for x in range(3, limit):
+    # x is the length of one side
+    tot = x*x
+    start = 2 if x % 2 == 0 else 1
+    for y in range(x-2, start-1, -2):
+        # y is the length of the inner square
+        v = tot - y*y
+        # v is the number of tiles used
+        if v > max_tiles:
+            break
+        
+        if v in count:
+            count[v] += 1
+        else:
+            count[v] = 1
+        #print x, y, 'using', tot - y*y, 'tiles'
+
+L = {}
+for i in count:
+    if count[i] in L:
+        L[count[i]] += 1
+    else:
+        L[count[i]] = 1
+
+print(sum([L[i] for i in range(11) if i in L]))
