30e139951e806debc347e69434adfc48ea73bf9b
Heuristic <codemonkey2468@hotmail.com>
2021-11-26 12:49:30 -0500

    finish problem 72

--- Problem72.py
+++ Problem72.py
@@ -2,3 +2,12 @@
 counting fractions
 """
 
+import time
+from pe070 import totient_array
+
+
+if __name__ == '__main__':
+    start = time.clock_gettime()
+    N = 1000000
+    print(sum(totient_array(N)) - 1)
+    print('Runtime is ', time.clock_gettime() - start)
