b8953146b589ab4e6d2f2684dceb02c3a7b7ee17
Heuristic <codemonkey2468@hotmail.com>
2021-11-29 18:20:41 -0500

    finish problem 79

--- Problem79.py
+++ Problem79.py
@@ -2,3 +2,20 @@
 Passcode derivation
 """
 
+
+if __name__ == '__main__':
+    after = [set() for x in range(10)]
+    before = [set() for x in range(10)]
+
+    f = open('pe079.txt')
+    for line in f:
+        num = line[0:3]
+        for i in range(0, 2):
+            for j in range(i + 1, 3):
+                after[int(num[i])].add(int(num[j]))
+                before[int(num[j])].add(int(num[i]))
+    for i in range(10):
+        print('after', i, ':', after[i])
+    print('=================================')
+    for i in range(10):
+        print('before', i, ':', before[i])
