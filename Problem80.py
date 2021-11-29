3f50d2d775cd01d7dc831264b68bd075286b31d3
Heuristic <codemonkey2468@hotmail.com>
2021-11-28 21:05:22 -0500

    finish problem 80

--- Problem80.py
+++ Problem80.py
@@ -2,3 +2,13 @@
 Square root digital expansion
 """
 
+from decimal import Decimal, getcontext
+
+
+if __name__ == '__main__':
+    getcontext().prec = 102
+    answer = 0
+    for i in range(1, 101):
+        if Decimal(i).sqrt() % 1 != 0:
+            answer += sum(int(c) for c in str(Decimal(i).sqrt() * 10 ** 100)[:100])
+    print(answer)
