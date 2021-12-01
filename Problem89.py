f843283c1915b6e8c18f4381b090f36541aa71d6
Heuristic <codemonkey2468@hotmail.com>
2021-12-01 18:44:12 -0500

    finish problem 89

--- Problem89.py
+++ Problem89.py
@@ -2,3 +2,17 @@
 Roman numerals
 """
 
+if __name__ == '__main__':
+    f = open('pe089.txt')
+    src = ''
+    r = ''
+    for line in f:
+        src += line
+        s = line.replace('DCCCC', 'CM')
+        s = s.replace('CCCC', 'CD')
+        s = s.replace('LXXXX', 'XC')
+        s = s.replace('XXXX', 'XL')
+        s = s.replace('VIIII', 'IX')
+        s = s.replace('IIII', 'IV')
+        r += s
+    print(len(src) - len(r))
