d7a4297f9b870b14182888fc4ba3979ce294c83c
Heuristic <codemonkey2468@hotmail.com>
2021-12-06 12:35:49 -0500

    finish problem 116

--- Problem116.py
+++ Problem116.py
@@ -1,3 +1,14 @@
 """
 Red, green or blue tiles
 """
+
+def f(m, n):
+    ways = [0] * (n + 1)
+    for i in range(m):
+        ways[i] = 1
+    for i in range(m, n + 1):
+        ways[i] += ways[i - 1] + ways[i - m]
+    return ways[n] - 1
+
+if __name__ == '__main__':
+    print(f(2, 50) + f(3, 50) + f(4, 50))
