087984ec844ab74457fd8477d9b6edc8acbb2cd1
Heuristic <codemonkey2468@hotmail.com>
2021-12-07 12:43:36 -0500

    finish problem 151

--- Problem151.py
+++ Problem151.py
@@ -1,3 +1,32 @@
 """
 Paper sheets of standard sizes: an expected-value problem
 """
+
+import eulerlib
+
+
+def cal():
+	ans = get_expected_singles((1,)) - 2
+	return f"{ans:.6f}"
+
+
+@eulerlib.memoize
+def get_expected_singles(state):
+	result = 0.0
+	if len(state) > 0:
+		for i in range(len(state)):
+			tempstate = list(state)
+			sheet = state[i]
+			del tempstate[i]
+			for j in range(sheet + 1, 6):
+				tempstate.append(j)
+			tempstate.sort()
+			result += get_expected_singles(tuple(tempstate))
+		result /= len(state)
+		if len(state) == 1:
+			result += 1.0
+	return result
+
+
+if __name__ == "__main__":
+	print(cal())
