0fd3df49e48ea0d709c23a42c21f6f318fa1dff3
Heuristic <codemonkey2468@hotmail.com>
2021-12-09 15:59:57 -0500

    finish problem 162

--- Problem162.py
+++ Problem162.py
@@ -1,3 +1,11 @@
 """
 Hexadecimal numbers
 """
+
+def cal():
+	answer = sum((15 * 16**(n-1) - 43 * 15**(n-1) + 41 * 14**(n-1) - 13**n) for n in range(1, 17))
+	return f"{answer:X}"
+
+
+if __name__ == "__main__":
+	print(cal())
