d9ecd9738814361a005df28d6f433d6319c45873
Heuristic <codemonkey2468@hotmail.com>
2021-12-13 13:17:18 -0500

    finish problem 47

--- Problem47.py
+++ Problem47.py
@@ -18,3 +18,23 @@ What is the first of these numbers?
 
 """""
 
+def distinct_prime_factor(n):
+    cnt = 0
+    start = 2
+    end = int(n ** 0.5)
+    while start <= end:
+        if n % start == 0:
+            cnt += 1
+            while n % start == 0:
+                n //= start
+        start += 1
+        end = int(n ** 0.5)
+    if n > 1:
+        cnt += 1
+    return cnt
+
+if __name__ == '__main__':
+    i = 2
+    while not distinct_prime_factor(i) == distinct_prime_factor(i + 1) == distinct_prime_factor(i + 2) == distinct_prime_factor(i + 3) == 4:
+        i += 1
+    print(i)
