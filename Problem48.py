e4e261d9a810cfb0e44fdc8b5b136e320614adb7
Heuristic <codemonkey2468@hotmail.com>
2021-12-13 13:20:18 -0500

    finish problem 48

--- Problem48.py
+++ Problem48.py
@@ -7,3 +7,8 @@ Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1^0001000.
 
 """""
 
+if __name__ == '__main__':
+    answer = 0
+    for i in range(1, 1001):
+        answer += i ** i
+    print(answer % (10 ** 10))
