5b253bcc646c414b8f1ae0d35ddbed86fc6f81b6
Heuristic <codemonkey2468@hotmail.com>
2021-12-14 12:57:47 -0500

    finish problem 52

--- Problem52.py
+++ Problem52.py
@@ -6,3 +6,16 @@ Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x,
 contain the same digits.
 
 """""
+
+import time
+
+if __name__ == '__main__':
+    start = time.clock()
+    x = 1
+    while True:
+        if sorted(str(x)) == sorted(str(2 * x)) == sorted(str(3 * x)) == \
+                sorted(str(4 * x)) == sorted(str(5 * x)) == sorted(str(6 * x)):
+            break
+        x += 1
+    print(x)
+    print('Runtime is ', time.clock() - start)
