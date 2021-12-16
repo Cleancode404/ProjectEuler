a884b6856c1916390aa932ff6865975e568d36a5
Heuristic <codemonkey2468@hotmail.com>
2021-12-16 13:34:13 -0500

    finish problem 36

--- Problem36.py
+++ Problem36.py
@@ -12,3 +12,10 @@ less than one million, which are palindromic in base 10 and base 2.
 
 """""
 
+def is_palindrome(s):
+    return s == s[::-1]
+
+if __name__ == '__main__':
+    answer = 0
+    answer += sum(i for i in range(1000000) if is_palindrome(str(i)) and is_palindrome(bin(i)[2:]))
+    print(answer)
