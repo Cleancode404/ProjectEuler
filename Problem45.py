bd20fc17cdbf46d2792bf79a23b559c5044d4d68
Heuristic <codemonkey2468@hotmail.com>
2021-12-17 12:26:26 -0500

    finish problem 45

--- Problem45.py
+++ Problem45.py
@@ -12,4 +12,22 @@ It can be verified that T285 = P165 = H143 = 40755.
 Find the next triangle number that is also pentagonal and hexagonal.
 
 """""
+import time 
 
+def project_euler():
+    p, h = 165, 144
+    hen = set()
+    next_pen = next_hen = 0
+    while next_pen not in hen:
+        next_pen = p * (3 * p - 1) // 2
+        p += 1
+        while next_hen < next_pen:
+            h += 1
+            next_hen = h * (2 * h - 1)
+            hen.add(next_hen)
+    return next_hen
+
+if __name__ == '__main__':
+    start = time.clock()
+    print(project_euler())
+    print('Runtime is ', time.clock() - start)
