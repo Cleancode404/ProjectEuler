0c2bfb2ad81b862127c2dc2a75e3555d3654fb8b
Heuristic <codemonkey2468@hotmail.com>
2021-12-21 14:09:05 -0500

    finish problem 34

--- Problem34.py
+++ Problem34.py
@@ -8,3 +8,24 @@ the sum of the factorial of their digits.
 Note: As 1! = 1 and 2! = 2 are not sums they are not included.
 
 """""
+
+import time
+
+if __name__ == '__main__':
+    start = time.clock()
+    factorial = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880]
+    n = 1
+    while n * factorial[9] >= 10 ** n - 1:
+        n += 1
+    res = 0
+    for x in range(10, 10 ** n):
+        s = 0
+        temp = x
+        while temp > 0:
+            s += factorial[temp % 10]
+            temp //= 10
+        if s == x:
+            print(x)
+            res += s
+    print(res)
+    print('Runtime is ', time.clock() - start)
