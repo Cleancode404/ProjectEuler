02f1815e772ea40b5ee3837e437a08ad51df018b
Heuristic <codemonkey2468@hotmail.com>
2021-12-23 12:51:01 -0500

    finish problem 41

--- Problem41.py
+++ Problem41.py
@@ -8,3 +8,20 @@ What is the largest n-digit pandigital prime that exists?
 
 """""
 
+import time
+
+if __name__ == '__main__':
+    start = time.clock()
+    N = 7654321
+    prime = [True] * (N + 1)
+    prime[0] = prime[1] = False
+    for i in range(2, int(N ** 0.5) + 1):
+        if prime[i]:
+            for j in range(i * i, N + 1, i):
+                prime[j] = False
+    for i in range(7654321, 0, -1):
+        if prime[i] and sorted(str(i)) == (['1', '2', '3', '4', '5', '6', '7']
+                                           or ['1', '2', '3', '4']):
+            print(i)
+            break
+    print('Runtime is ', time.clock() - start)
