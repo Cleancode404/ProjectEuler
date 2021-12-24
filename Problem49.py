6434b2f5fc1dae4b7c46ff86b604c53644f2cf93
Heuristic <codemonkey2468@hotmail.com>
2021-12-24 14:15:15 -0500

    finish problem 49

--- Problem49.py
+++ Problem49.py
@@ -15,3 +15,30 @@ What 12-digit number do you form
 by concatenating the three terms in this sequence?
 
 """""
+
+def number_array(n):
+    array = []
+    for i in range(len(str(n))):
+        array.append(str(n)[i])
+    return sorted(array)
+
+if __name__ == '__main__':
+    N = 10000
+    prime = [True] * N
+    prime[0] = prime[1] = False
+    for i in range(2, int(N ** 0.5) + 1):
+        if prime[i]:
+            for j in range(i * i, N, i):
+                prime[j] = False
+    pp = []
+    for i in range(1000, N):
+        if prime[i]:
+            pp.append(i)
+
+    for i in range(len(pp)):
+        for j in range(i + 1, len(pp)):
+            if 2 * pp[j] - pp[i] >= 10000:
+                break
+            if prime[2 * pp[j] - pp[i]] and number_array(pp[i]) == \
+                    number_array(pp[j]) == number_array(2 * pp[j] - pp[i]):
+                print(pp[i], pp[j], 2 * pp[j] - pp[i])
